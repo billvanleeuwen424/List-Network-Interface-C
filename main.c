f1cf9412623658707309cb894dc221dc880769bb
billvanleeuwen424@gmail.com <billvanleeuwen424@gmail.com>
2022-04-09 15:34:10 -0400

    get address

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+#include <netdb.h>
+#include <ifaddrs.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+
+#include <errno.h>
+#include <string.h>
+
 int main(){
-    
+    //a linked list of the addresses
+    struct ifaddrs *addresses;
+
+    //get the list
+    if((getifaddrs(&addresses)) == -1){
+        fprintf(stderr, "get address failure. %s",  strerror(errno));
+        exit(-1);
+    }
+
 }
